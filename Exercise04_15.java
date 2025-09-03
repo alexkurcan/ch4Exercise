cc80b618bc1a90bfa4c13966115e1b7bd8f628c1
alexkurcan <alxkurcan56@outlook.com>
2025-09-03 09:26:09 -0400

    fixing name

--- Exercise04_15.java
+++ Exercise04_15.java
@@ -6,7 +6,7 @@
  */
 
 import java.util.Scanner;
-public class LetterFinder{
+public class Exercise04_15{
     public static void main(String[] args) {
             Scanner input = new Scanner(System.in);
 
